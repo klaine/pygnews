f157d96c4117ef1eeca4ee6acf3cd5c64ace7f9a
klaine <kajo.laine@legendat.org>
2013-04-23 09:19:08 +0300

    Create news.py
    Initial version.

---
+++ news.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import lxml.etree
+import urllib2
+import sys
+import codecs
+
+# Type './news.py' to show latest news
+# Type './news.py boston bombing' to show latest news on Boston bombings or use any keywords as arguments
+# I like to copy this to /usr/bin/ to make usage easier
+
+query = '%20'.join(sys.argv[1:])
+url = 'https://news.google.com/news/feeds?output=rss&num=10&q='+query
+content = urllib2.urlopen(url)
+parsed = lxml.etree.parse(content)
+for item in parsed.xpath('.//item'):
+  if len(query) > 0:
+    print item[3].text + ' | ' + item[0].text.encode('ascii','replace') + '\n' + item[1].text.split('&url=')[1] + '\n'
+  else:
+    print item[4].text + ' | ' + item[0].text.encode('ascii','replace') + '\n' + item[1].text.split('&url=')[1] + '\n'
